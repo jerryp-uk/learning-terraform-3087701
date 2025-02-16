131e73182550fcaceb9ddd72c142f87868771dec
jerryp-uk <jerry.pringle@me.com>
2025-02-16 21:44:16 +0000

    From doc

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "aws_ami" "app_ami" {
 
   owners = ["979382823631"] # Bitnami
 }
- 
+
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -23,7 +23,6 @@ module "blog_vpc" {
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -31,30 +30,32 @@ module "blog_vpc" {
 }
 
 
-
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.1.0"
-  
+  version = "6.5.2"
+
   name = "blog"
-  min_size = 1
-  max_size = 2
 
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_groups["ex-instance"].arn
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-
-  image_id                 = data.aws_ami.app_ami.id
   instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name            = "blog-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
@@ -63,7 +64,6 @@ module "blog_alb" {
       backend_port     = 80
       target_type      = "instance"
     }
-
   ]
 
   http_tcp_listeners = [
@@ -74,10 +74,8 @@ module "blog_alb" {
     }
   ]
 
-
   tags = {
-    Environment = "Development"
-    Project     = "Example"
+    Environment = "dev"
   }
 }
 
